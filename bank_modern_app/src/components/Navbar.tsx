fe7dc274536c1188b9384f8d57d76b36d4ca8d09
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-27 22:02:26 +0100

    add toggle hook and add state to burger menu

--- bank_modern_app/src/components/Navbar.tsx
+++ bank_modern_app/src/components/Navbar.tsx
@@ -4,6 +4,9 @@ import { close, logo, menu } from '../assets'
 import { navLinks } from '../constants'
 
 const Navbar = () => {
+
+    const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="w-full flex py-6 justofy-between items-center navbar">
         <img src={logo} alt="hoobank" className="w-[124px] h-[32px]"/>
@@ -19,6 +22,17 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img src={toggle ? close : menu} 
+          alt="menu" 
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle((prev) => !prev)}
+          />
+
+          <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+            
+          </div>
+        </div>
     </nav>
   )
 }
