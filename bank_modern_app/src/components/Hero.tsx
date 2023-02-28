f523094dd62d01c2d628001ac635d919c15e40dd
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-02-28 17:07:42 +0100

    add GetStarted component

--- bank_modern_app/src/components/Hero.tsx
+++ bank_modern_app/src/components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from '../styles';
 import { discount, robot } from '../assets';
+import GetStarted from './GetStarted';
 
 const Hero = () => (
    
@@ -18,8 +19,13 @@ const Hero = () => (
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
             The Next <br className="sm:block hidden" />
             {"  "} <span className="text-gradient">Generation</span> {" "}
-            Payment Method</h1>
+            Payment Method
+          </h1>
+          <div className="ss:flex hidden md:mr-4 mr-0">
+            <GetStarted />
+          </div>
         </div>
+        
       </div>
     </section>
   )
