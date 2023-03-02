99556f6c9e2a9d52cfca6726adf7cfa8660a6cf1
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-02 11:14:35 +0100

    add FeatureCard component with icons

--- bank_modern_app/src/components/Business.tsx
+++ bank_modern_app/src/components/Business.tsx
@@ -2,6 +2,14 @@ import { features } from "../constants";
 import styles, { layout } from "../styles";
 import Button from "./Button";
 
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length -1 ? "mb-6" : "mb-0"} feature-card`} >
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain"/>
+    </div>
+  </div>
+)
+
 const Business = () =>  (
   <section id="features" className={layout.section}>
     <div className={layout.sectionInfo}>
@@ -16,6 +24,12 @@ const Business = () =>  (
       </p>
 
       <Button styles="mt-10" />
+    </div>
+
+    <div className={`${layout.sectionImg} flex-col`}>
+      {features.map((feature, index) => (
+        <FeatureCard key={feature.id} {...feature} index={index}/>
+      ))}
     </div>
   </section>
 );
