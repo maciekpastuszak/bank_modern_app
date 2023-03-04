d10356253dbf30782a051c5e195738196abc5605
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-04 22:21:40 +0100

    add h2 and paragrapf to Billing

--- bank_modern_app/src/components/Billing.tsx
+++ bank_modern_app/src/components/Billing.tsx
@@ -11,6 +11,19 @@ const Billing = () => (
       <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
       {/* gradient end */}
     </div>
+
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Easily control your <br className="sm:block hidden" /> billing &
+        invoicing
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
+        aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
+        placerat.
+      </p>
+
+    </div>
   </section>
 )
 
