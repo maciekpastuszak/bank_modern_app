8f07c0b890d1f763668b00d4fbe25772618b7473
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-04 22:24:29 +0100

    develop cardDeal component

--- bank_modern_app/src/components/CardDeal.tsx
+++ bank_modern_app/src/components/CardDeal.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
 
 export default CardDeal
