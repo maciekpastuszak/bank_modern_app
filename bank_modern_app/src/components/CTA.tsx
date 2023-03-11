6a2cc6cc1c3c482fab49557dda200a220ec66ee1
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-11 16:28:46 +0100

    add markup to CTA component

--- bank_modern_app/src/components/CTA.tsx
+++ bank_modern_app/src/components/CTA.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import style from '../styles';
+import Button from './Button';
 
-const CTA = () => {
-  return (
-    <div>CTA</div>
+const CTA = () => (
+    <section>
+        <div>
+          <h2>Let's try our service now!</h2>
+          <p>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+        </div>
+
+        <div>
+          <Button />
+        </div>
+    </section>
   )
-}
 
 export default CTA
