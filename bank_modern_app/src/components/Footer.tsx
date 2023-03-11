b2a248e657f0c923af3e6ed38c50158b13e7adae
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-11 16:40:58 +0100

    initiate Footer component markup

--- bank_modern_app/src/components/Footer.tsx
+++ bank_modern_app/src/components/Footer.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import styles from '../styles';
+import { logo } from '../assets';
+import { footerLinks, socialMedia } from '../constants';
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
+const Footer = () => (
+    <section className={`${styles.flexCenter} ${styles.paddingY} fle-col`}>
+      <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+
+      </div>
+    </section>
   )
-}
+
 
 export default Footer
